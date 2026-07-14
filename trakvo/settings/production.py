e3c9a08af78e3b1a270f79ae91a21dad21269afd
Aman Pandit <Aman Pandit>
2026-07-14 09:32:54 +0530

    Add CSRF_TRUSTED_ORIGINS setting

--- trakvo/settings/production.py
+++ trakvo/settings/production.py
@@ -4,6 +4,8 @@ from decouple import config
 
 DEBUG = False
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='').split(',')
+CSRF_TRUSTED_ORIGINS = [url.strip() for url in CSRF_TRUSTED_ORIGINS if url.strip()]
 
 DATABASES = {
     'default': dj_database_url.config(
